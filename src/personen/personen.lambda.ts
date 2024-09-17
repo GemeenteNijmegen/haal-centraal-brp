1ed6d52078a1ef60cb14369486c8b2cc43845113
Martijn van Dijk <m.van.dijk@nijmegen.nl>
2024-09-17 15:57:29 +0200

    feat: add all types

--- src/personen/personen.lambda.ts
+++ src/personen/personen.lambda.ts
@@ -1,3 +1,142 @@
+import { Bsn, AWS } from '@gemeentenijmegen/utils';
+
 export async function handler (event: any, _context: any):Promise<any> {
-  return Response.json({ Personen: 'Test' });
+  console.log(event);
 };
+
+export async function callHaalCentraal(content: string) {
+  const endpoint = 'https://proefomgeving.haalcentraal.nl/haalcentraal/api/brp';
+  const apiKey = await AWS.getSecret(process.env.BRP_API_KEY!);
+
+  const response = await fetch(endpoint + '/personen',
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'X-API-KEY': apiKey,
+      },
+      body: content,
+    });
+
+  const data = await response.json() as Promise<any>;
+
+  if ((await data).personen[0].overlijden) {
+    throw new Error('Persoon lijkt overleden');
+  } else if ((await data).personen[0]) {
+    return (await data).personen[0];
+  }
+}
+
+export async function zoekMetGeslachtsnaamEnGeboortedatum() {
+
+  const content = {
+    type: 'ZoekMetGeslachtsnaamEnGeboortedatum',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    gemeenteVanInschrijving: '0518',
+    inclusiefOverledenPersonen: true,
+    geboortedatum: '1964-09-24',
+    geslachtsnaam: 'Vries',
+    geslacht: 'V',
+    voorvoegsel: 'de',
+    voornamen: 'Dirk',
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+}
+
+export async function zoekMetNaamEnGemeenteVanInschrijving() {
+
+  const content = {
+    type: 'ZoekMetNaamEnGemeenteVanInschrijving',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    gemeenteVanInschrijving: '0518',
+    inclusiefOverledenPersonen: true,
+    geslacht: 'V',
+    geslachtsnaam: 'Vries',
+    voorvoegsel: 'de',
+    voornamen: 'Dirk',
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+
+}
+
+export async function raadpleegMetBurgerservicenummer() {
+
+  const aBsn = new Bsn('12345678');
+
+  const content = {
+    type: 'RaadpleegMetBurgerservicenummer',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    burgerservicenummer: [aBsn.bsn],
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+}
+
+export async function zoekMetPostcodeEnHuisnummer() {
+
+  const content = {
+    type: 'ZoekMetPostcodeEnHuisnummer',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    gemeenteVanInschrijving: '0518',
+    inclusiefOverledenPersonen: true,
+    huisletter: 'a',
+    huisnummer: 14,
+    huisnummertoevoeging: 'bis',
+    postcode: '2341SX',
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+}
+
+export async function zoekMetStraatHuisnummerEnGemeenteVanInschrijving() {
+
+  const content = {
+    type: 'ZoekMetStraatHuisnummerEnGemeenteVanInschrijving',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    gemeenteVanInschrijving: '0518',
+    inclusiefOverledenPersonen: true,
+    huisletter: 'a',
+    huisnummer: 14,
+    huisnummertoevoeging: 'bis',
+    straat: 'Tulpstraat',
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+
+}
+
+export async function zoekMetNummeraanduidingIdentificatie() {
+
+  const content = {
+    type: 'ZoekMetNummeraanduidingIdentificatie',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    gemeenteVanInschrijving: '0518',
+    inclusiefOverledenPersonen: true,
+    nummeraanduidingIdentificatie: '0518200000366054',
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+
+}
+
+export async function zoekMetAdresseerbaarObjectIdentificatie() {
+
+  const content = {
+    type: 'ZoekMetAdresseerbaarObjectIdentificatie',
+    fields: ['aNummer', 'adressering', 'burgerservicenummer', 'datumEersteInschrijvingGBA', 'datumInschrijvingInGemeente', 'europeesKiesrecht', 'geboorte', 'gemeenteVanInschrijving', 'geslacht', 'gezag', 'immigratie', 'indicatieCurateleRegister', 'indicatieGezagMinderjarige', 'kinderen', 'leeftijd', 'naam', 'nationaliteiten', 'ouders', 'overlijden', 'partners', 'uitsluitingKiesrecht', 'verblijfplaats', 'verblijfstitel', 'verblijfplaatsBinnenland', 'adresseringBinnenland'],
+    gemeenteVanInschrijving: '0518',
+    inclusiefOverledenPersonen: true,
+    adresseerbaarObjectIdentificatie: '0226010000038820',
+  };
+
+  await callHaalCentraal(JSON.stringify(content) );
+
+}
