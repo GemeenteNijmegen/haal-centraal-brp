3a7bd753c50ac8045f448f19b464a731d3de7300
Martijn van Dijk <m.van.dijk@nijmegen.nl>
2024-10-24 16:04:03 +0200

    old ansi version

--- .projenrc.js
+++ .projenrc.js
@@ -2,14 +2,15 @@ const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/projen-project-typ
 const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  devDeps: ['@gemeentenijmegen/projen-project-type', '@testcontainers/localstack'],
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
   name: 'haal-centraal-brp',
 
   deps: ['@gemeentenijmegen/aws-constructs',
     '@gemeentenijmegen/utils',
     'https',
     'aws-sdk',
-    'node-fetch'], /* Runtime dependencies of this module. */
+    'node-fetch',
+    'wrap-ansi@6'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
