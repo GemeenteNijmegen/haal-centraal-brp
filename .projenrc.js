dda01e8a55017c5bc10221364a4997e65a9d41bb
Martijn van Dijk <m.van.dijk@nijmegen.nl>
2024-11-12 09:37:56 +0100

    yarn upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -9,8 +9,7 @@ const project = new GemeenteNijmegenCdkApp({
     '@gemeentenijmegen/utils',
     'https',
     'aws-sdk',
-    'node-fetch',
-    'wrap-ansi@6'], /* Runtime dependencies of this module. */
+    'node-fetch'], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
   depsUpgradeOptions: {
