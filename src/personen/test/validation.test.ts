09c2724c0143834de36ea300d2e29b9d4b8e4258
Wieteke <w.kremer@nijmegen.nl>
2025-02-03 12:10:39 +0100

    chore: manual upgrade of dependencies with small fix

--- src/personen/test/validation.test.ts
+++ src/personen/test/validation.test.ts
@@ -1,15 +1,21 @@
 import { DynamoDB } from 'aws-sdk';
-import { validateFields, getAllowedFields, handler } from '../personen.lambda';
+import { initSecrets, PersonenSecrets } from '../initSecrets';
+import { validateFields, getAllowedFields } from '../personen.lambda'; // Handler import done later on to mock initSecrets
+
+
+jest.mock('node-fetch', () => jest.fn());
+jest.mock('../initSecrets'); // Set mockResolve seperately to prevent unused import
+
+
+let mockDBGet = jest.fn().mockImplementation(() => ({
+  promise: jest.fn().mockResolvedValue({ Item: { fields: { values: ['field1', 'field2'] } } }),
+}));
 
 jest.mock('aws-sdk', () => {
-  const mockGet = jest.fn().mockImplementation(() => ({
-    promise: jest.fn().mockResolvedValue({ Item: { fields: { values: ['field1', 'field2'] } } }),
-  }));
-
   return {
     DynamoDB: {
       DocumentClient: jest.fn(() => ({
-        get: mockGet,
+        get: mockDBGet,
       })),
     },
   };
@@ -47,35 +53,32 @@ describe('getAllowedFields', () => {
   });
 
   it('should handle missing data gracefully', async () => {
-    jest.mock('aws-sdk', () => ({
-      DynamoDB: {
-        DocumentClient: jest.fn(() => ({
-          get: jest.fn(() => ({
-            promise: jest.fn().mockResolvedValue({}),
-          })),
-        })),
-      },
+    mockDBGet.mockImplementationOnce(() => ({
+      promise: jest.fn().mockResolvedValue({}), // No Item field
     }));
-
     const idTable = new DynamoDB.DocumentClient();
     const apiKey = 'test-api-key'; //TODO
-
     process.env.ID_TABLE_NAME = 'TestTable'; //TODO
-
     const fields = await getAllowedFields(apiKey, idTable);
     expect(fields).toBeUndefined();
   });
 });
 
 describe('handler', () => {
+  let handler: any;
+  beforeEach(async () => {
+    (initSecrets as jest.Mock).mockResolvedValue({} as any as PersonenSecrets);
+    // Re-import handler to use mocked secrets, move more to the top if more handler calls are made
+    jest.resetModules();
+    const importedModule = (await import('../personen.lambda')); // should load after mock secrets, otherwise cannot set mock above handler
+    handler = importedModule.handler;
+  });
   it('should return 403 for invalid fields', async () => {
     const event = {
       body: JSON.stringify({ fields: ['invalidField'] }),
       requestContext: { identity: { apiKey: 'test-api-key' } }, //TODO
     };
-
     const result = await handler(event);
-
     expect(result.statusCode).toBe('403');
     expect(result.body).toBe('Mismatch in application/profile');
   });
