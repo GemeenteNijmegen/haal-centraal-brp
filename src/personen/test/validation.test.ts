7ed05e836655b22d5ae6d817d909d7811acd5288
MartijnvDijk <m.van.dijk@nijmegen.nl>
2025-02-10 16:02:07 +0100

    feat: clean validation test

--- src/personen/test/validation.test.ts
+++ src/personen/test/validation.test.ts
@@ -25,7 +25,7 @@ describe('validateFields', () => {
   it('should return true when all fields are allowed', async () => {
     const idTable = new DynamoDB.DocumentClient();
     const receivedFields = ['field1'];
-    const applicationId = 'app-id'; //TODO
+    const applicationId = 'app-id-test';
 
     const result = await validateFields(receivedFields, applicationId, idTable);
     expect(result).toBe(true);
@@ -34,7 +34,7 @@ describe('validateFields', () => {
   it('should return false when some fields are not allowed', async () => {
     const idTable = new DynamoDB.DocumentClient();
     const receivedFields = ['field3'];
-    const applicationId = 'app-id'; //TODO
+    const applicationId = 'app-id-test';
 
     const result = await validateFields(receivedFields, applicationId, idTable);
     expect(result).toBe(false);
@@ -44,9 +44,9 @@ describe('validateFields', () => {
 describe('getAllowedFields', () => {
   it('should return allowed fields for a given API key', async () => {
     const idTable = new DynamoDB.DocumentClient();
-    const apiKey = 'test-api-key'; //TODO
+    const apiKey = 'test-api-key';
 
-    process.env.ID_TABLE_NAME = 'TestTable'; //TODO
+    process.env.ID_TABLE_NAME = 'TestTable';
 
     const fields = await getAllowedFields(apiKey, idTable);
     expect(fields).toEqual(['field1', 'field2']);
@@ -57,8 +57,8 @@ describe('getAllowedFields', () => {
       promise: jest.fn().mockResolvedValue({}), // No Item field
     }));
     const idTable = new DynamoDB.DocumentClient();
-    const apiKey = 'test-api-key'; //TODO
-    process.env.ID_TABLE_NAME = 'TestTable'; //TODO
+    const apiKey = 'test-api-key';
+    process.env.ID_TABLE_NAME = 'TestTable';
     const fields = await getAllowedFields(apiKey, idTable);
     expect(fields).toBeUndefined();
   });
@@ -76,7 +76,7 @@ describe('handler', () => {
   it('should return 403 for invalid fields', async () => {
     const event = {
       body: JSON.stringify({ fields: ['invalidField'] }),
-      requestContext: { identity: { apiKey: 'test-api-key' } }, //TODO
+      requestContext: { identity: { apiKey: 'test-api-key' } },
     };
     const result = await handler(event);
     expect(result.statusCode).toBe('403');
