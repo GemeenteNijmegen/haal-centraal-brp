4559dc73fb46359c91137519b383214533be3a47
MartijnvDijk <m.van.dijk@nijmegen.nl>
2025-05-08 16:25:38 +0200

    feat: create subset request

---
+++ src/personen/subset/subset.lambda.ts
@@ -0,0 +1,79 @@
+import { Tracer } from '@aws-lambda-powertools/tracer';
+import type { Subsegment } from 'aws-xray-sdk-core';
+import { callHaalCentraal } from '../callHaalCentraal';
+import { initSecrets, PersonenSecrets } from '../initSecrets';
+import { validateFields } from '../validateFields';
+
+let secrets: PersonenSecrets;
+let init = initSecrets();
+let tracer: Tracer | undefined;
+
+if (process.env.TRACING_ENABLED) {
+  tracer = new Tracer({ serviceName: 'haalcentraal-personen-subset', captureHTTPsRequests: true });
+}
+
+export async function handler(event: any): Promise<any> {
+  const segment = tracer?.getSegment(); // This is the facade segment (the one that is created by AWS Lambda)
+  let subsegment: Subsegment | undefined;
+  if (tracer && segment) {
+    // Create subsegment for the function & set it as active
+    subsegment = segment.addNewSubsegment(`## ${process.env._HANDLER}`);
+    tracer.setSegment(subsegment);
+  }
+  tracer?.annotateColdStart();
+  tracer?.addServiceNameAnnotation();
+
+  // Add the API key as an annotation
+  const apiKeyId = event.requestContext.identity.apiKeyId;
+  tracer?.putAnnotation('API Key ID', apiKeyId);
+
+  try {
+    if (!secrets) {
+      secrets = await init;
+    }
+
+    const fields = [
+      'kinderen',
+      'leeftijd',
+      'partners',
+    ];
+
+    const apiKey = event.requestContext.identity.apiKey;
+    const validProfile = await validateFields(fields, apiKey);
+
+    if (validProfile) {
+      // Search...
+      const body = await jsonBody(fields, event.requestContext.identity.bsn);
+      return await callHaalCentraal(body, secrets);
+    } else {
+      return {
+        statusCode: '403', //Forbidden
+        body: 'Mismatch in application/profile',
+        headers: { 'Content-Type': 'text/plain' },
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+      headers: { 'Content-Type': 'text/plain' },
+    };
+  } finally {
+    if (tracer && segment && subsegment) {
+      // Close subsegment (the AWS Lambda one is closed automatically)
+      subsegment.close();
+      // Set back the facade segment as active again
+      tracer?.setSegment(segment);
+    }
+  }
+}
+
+async function jsonBody(fields: string[], bsn: string[]): Promise<string> {
+  const body = {
+    type: 'RaadpleegMetBurgerservicenummer',
+    fields: fields,
+    burgerservicenummer: bsn,
+  };
+  return JSON.stringify(body);
+}
